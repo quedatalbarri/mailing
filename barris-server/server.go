a83f6e78bca160c85ab78d251d9cb9be5f9d91d6
Laura Tristan <lauratrpolo@gmail.com>
2020-02-11 00:24:14 +0100

    code improvement

--- barris-server/server.go
+++ barris-server/server.go
@@ -8,40 +8,34 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-type (
-	barri struct {
-		Name string `json:"name"`
-	}
-)
+type barri struct {
+	Name string `json:"name"`
+}
 
-var seq = 1
-var xxx string
+func hello(c.echo.Context) error {
+	return c.String(http.StatusOK, "Welcome a barri server!")
+}
 
-func createBarri(c echo.Context) error {
-	fmt.Println("Funcion createBarri")
-	u := &barri{
-		Name: xxx,
-	}
-	if err := c.Bind(u); err != nil {
+func addBarri(c echo.Context) error {
+	fmt.Println("Funcion addBarri")
+	b := &barri{}
+	if err := c.Bind(b); err != nil {
 		return err
 	}
-	fmt.Println("Barri: ", u.Name)
+	fmt.Println("Barri: ", b.Name)
 	//TODO add barrio to a database
-	return c.JSON(http.StatusCreated, u)
+	return c.JSON(http.StatusCreated, b)
 }
 
 func main() {
-	fmt.Println("Funcion main")
-
 	e := echo.New()
 
-	// CORS restricted
-	// Allows requests
+	// CORS restricted- Allows requests
 	e.Use(middleware.CORS())
+	
 	//ROUTES
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Welcome a barri server!")
-	})
-	e.POST("/addBarri", createBarri)
+	e.GET("/", hello)
+	e.POST("/addBarri", addBarri)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }

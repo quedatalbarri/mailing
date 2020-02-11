a3d9ce41335bda8b9932ccd279ac84285ff07a71
Laura Tristan <lauratrpolo@gmail.com>
2020-02-11 01:02:31 +0100

    connect to local postgres database

--- barris-server/go.mod
+++ barris-server/go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/echo/v4 v4.1.14 // indirect
+	github.com/lib/pq v1.3.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	golang.org/x/crypto v0.0.0-20200208060501-ecb85df21340 // indirect
 	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
--- barris-server/server.go
+++ barris-server/server.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "12345"
+	dbname   = "quedatalbarri"
 )
 
 type barri struct {
 	Name string `json:"name"`
 }
 
-func hello(c.echo.Context) error {
+func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Welcome a barri server!")
 }
 
@@ -27,12 +38,40 @@ func addBarri(c echo.Context) error {
 	return c.JSON(http.StatusCreated, b)
 }
 
+// func addBarriToDB() {
+// 	sqlStatement := "INSERT INTO barris (name, salry,age)VALUES ($1, $2, $3)"
+// 	res, err := db.Query(sqlStatement, u.Name, u.Salary, u.Age)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	} else {
+// 		fmt.Println(res)
+// 		return c.JSON(http.StatusCreated, u)
+// 	}
+// }
+
+func connectToDatabase() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		panic(err)
+	} else {
+		fmt.Println("DB Connected...")
+	}
+}
+
 func main() {
+	connectToDatabase()
 	e := echo.New()
 
 	// CORS restricted- Allows requests
 	e.Use(middleware.CORS())
-	
+
 	//ROUTES
 	e.GET("/", hello)
 	e.POST("/addBarri", addBarri)
